f8be6fa4cc81005a59ea183a32e61f2098ec528e
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-01-09 04:04:15 +0900

    add solution finder

--- src/utils/mock/CollatzPuzzle.sol
+++ src/utils/mock/CollatzPuzzle.sol
@@ -12,7 +12,7 @@ contract CollatzPuzzle is IPuzzle {
     }
 
     /// @inheritdoc IPuzzle
-    function generate(address _seed) external pure returns (uint256) {
+    function generate(address _seed) public pure returns (uint256) {
         // The last 8 bits denote the number of times to apply the operation.
         // We want to apply it at least once, hence `| 1`.
         return uint256(keccak256(abi.encodePacked(_seed))) | 1;
@@ -23,16 +23,37 @@ contract CollatzPuzzle is IPuzzle {
         // Retrieve the last 8 bits.
         uint256 iterations = _start & 0xFF;
 
-        for (uint256 i = 0; i < iterations; ) {
+        for (uint256 i = 0; i < iterations;) {
             unchecked {
                 // Collatz Operation
                 if (_start & 1 == 0) _start >>= 1;
-                else                 _start = 3 * _start + 1;
+                else _start = 3 * _start + 1;
 
                 ++i;
             }
         }
 
         return _start == _solution;
+    }
+
+    /// @dev This function is just used as a util function in tests. DO NOT
+    /// include a function like this in your puzzle contract.
+    function solve(address _seed) external pure returns (uint256) {
+        uint256 start = generate(_seed);
+
+        // Retrieve the last 8 bits.
+        uint256 iterations = start & 0xFF;
+
+        for (uint256 i = 0; i < iterations;) {
+            unchecked {
+                // Collatz Operation
+                if (start & 1 == 0) start >>= 1;
+                else start = 3 * start + 1;
+
+                ++i;
+            }
+        }
+
+        return start;
     }
 }
