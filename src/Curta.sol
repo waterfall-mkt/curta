c9a4a3519524a5df4ed4665881de9b7bebc86537
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-01-11 04:03:37 +0900

    docs(Curta): fix comments in computePhase

--- src/Curta.sol
+++ src/Curta.sol
@@ -105,7 +105,7 @@ contract Curta is ICurta, FlagsERC721 {
         // Revert if submissions are closed.
         uint40 firstSolveTimestamp = puzzleData.firstSolveTimestamp;
         uint40 solveTimestamp = uint40(block.timestamp);
-        uint8 phase = computePhase(firstSolveTimestamp, solveTimestamp);
+        uint8 phase = _computePhase(firstSolveTimestamp, solveTimestamp);
         if (phase == 3) revert SubmissionClosed(_puzzleId);
 
         // Revert if the solution is incorrect.
@@ -261,15 +261,24 @@ contract Curta is ICurta, FlagsERC721 {
     /// before the puzzle has been solved, "Phase 1" refers to the period 2 days
     /// after the first solve, "Phase 2" refers to the period 3 days after the
     /// end of "Phase 1", and "Phase 3" is when submissions are closed.
-    function computePhase(uint40 _firstSolveTimestamp, uint40 _solveTimestamp)
+    function _computePhase(uint40 _firstSolveTimestamp, uint40 _solveTimestamp)
         internal
         pure
         returns (uint8 phase)
     {
+        // Equivalent to:
+        // if (_firstSolveTimestamp == 0) {
+        //     phase = 0;
+        // } else {
+        //     if (_solveTimestamp > _firstSolveTimestamp + SUBMISSION_LENGTH) {
+        //         phase = 3;
+        //     } else if (_solveTimestamp > _firstSolveTimestamp + PHASE_ONE_LENGTH) {
+        //         phase = 2;
+        //     } else {
+        //         phase = 1;
+        //     }
+        // }
         assembly {
-            // (_solveTimestamp > _firstSolveTimestamp)                              Phase 1 Over
-            //     + (_solveTimestamp > _firstSolveTimestamp + PHASE_ONE_LENGTH)     Phase 2 Over
-            //     + (_solveTimestamp > _firstSolveTimestamp + SUBMISSION_LENGTH)    Phase 3 Over
             phase :=
                 mul(
                     iszero(iszero(_firstSolveTimestamp)),
