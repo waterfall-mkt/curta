aaac49fb44a1a4db7aa73e8e196f0abb7fcbd990
e6f4e37l <66016924+e6f4e37l@users.noreply.github.com>
2023-02-28 13:22:28 -0500

    docs: document print utility script

--- script/PrintAuthorshipToken.s.sol
+++ script/PrintAuthorshipToken.s.sol
@@ -6,12 +6,26 @@ import { console } from "forge-std/Test.sol";
 
 import { AuthorshipToken } from "@/contracts/AuthorshipToken.sol";
 
-contract PrintAuthorshipTokenScriptScript is Script {
+/// @notice A script to print the token URI returned by `AuthorshipToken` for
+/// testing purposes.
+/// @dev Either comment out `ICurta(curta).hasUsedAuthorshipToken` in
+/// `AuthorshipToken` OR replace `curta` with a valid address and run the script
+/// with the corresponding RPC URL to fork with.
+contract PrintAuthorshipTokenScript is Script {
+    /// @notice The instance of `AuthorshipToken` that will be deployed after
+    /// the script runs. 
     AuthorshipToken internal authorshipToken;
 
+    /// @notice The address of the live deployed `Curta` contract.
+    /// @dev This can be a random address if
+    /// `ICurta(curta).hasUsedAuthorshipToken` has been commented out.
+    address constant CURTA = address(0xDEAD);
+
+    /// @notice Deploys an instance of `AuthorshipToken`, mints tokens #1, ...,
+    /// #9, then prints the token URI for 1 of them.
     function run() public {
         address[] memory authors;
-        authorshipToken = new AuthorshipToken(address(0xDEAF), 1 days, authors);
+        authorshipToken = new AuthorshipToken(CURTA, 1 days, authors);
         vm.warp(block.timestamp + 10 days);
 
         for (uint256 i; i < 10; ++i) {
