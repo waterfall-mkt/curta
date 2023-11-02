b6e0ae93bd58e32cb7deccefceddd349d35906a8
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-11-02 12:58:05 -0500

    chore: update tests

--- test/deploy/DeployBaseGoerli.t.sol
+++ test/deploy/DeployBaseGoerli.t.sol
@@ -5,16 +5,16 @@ import { Test } from "forge-std/Test.sol";
 
 import { AuthorshipToken } from "@/contracts/AuthorshipToken.sol";
 import { Curta } from "@/contracts/Curta.sol";
-import { DeployConstellation } from "@/script/deploy/DeployConstellation.s.sol";
+import { DeployBaseGoerli } from "@/script/deploy/DeployBaseGoerli.s.sol";
 
-/// @notice Tests the Constellation chain deploy script.
-contract DeployConstellationTest is Test {
+/// @notice Tests the Base Goerli deploy script. 
+contract DeployBaseGoerliTest is Test {
     // -------------------------------------------------------------------------
     // Contracts
     // -------------------------------------------------------------------------
 
-    /// @notice The mainnet deploy script.
-    DeployConstellation internal deployConstellation;
+    /// @notice The Base Goerli deploy script.
+    DeployBaseGoerli internal deployBaseGoerli;
 
     // -------------------------------------------------------------------------
     // Setup
@@ -23,8 +23,8 @@ contract DeployConstellationTest is Test {
     function setUp() public {
         vm.deal(vm.addr(vm.envUint("DEPLOYER_PRIVATE_KEY")), type(uint64).max);
 
-        deployConstellation = new DeployConstellation();
-        deployConstellation.run();
+        deployBaseGoerli = new DeployBaseGoerli();
+        deployBaseGoerli.run();
     }
 
     // -------------------------------------------------------------------------
@@ -34,44 +34,33 @@ contract DeployConstellationTest is Test {
     /// @notice Test that the addresses were set correctly in each contract's
     /// deploy.
     function test_AddressInitializationCorrectness() public {
+        assertEq(address(deployBaseGoerli.curta().flagRenderer()), address(deployBaseGoerli.flagRenderer()));
         assertEq(
-            address(deployConstellation.curta().flagRenderer()),
-            address(deployConstellation.flagRenderer())
+            address(deployBaseGoerli.curta().authorshipToken()), address(deployBaseGoerli.authorshipToken())
         );
-        assertEq(
-            address(deployConstellation.curta().authorshipToken()),
-            address(deployConstellation.authorshipToken())
-        );
-        assertEq(
-            deployConstellation.authorshipToken().curta(), address(deployConstellation.curta())
-        );
+        assertEq(deployBaseGoerli.authorshipToken().curta(), address(deployBaseGoerli.curta()));
     }
 
     /// @notice Test that the Authorship Token's issue length was set correctly.
     function test_authorshipTokenIssueLengthEquality() public {
-        assertEq(
-            deployConstellation.authorshipToken().issueLength(), deployConstellation.issueLength()
-        );
+        assertEq(deployBaseGoerli.authorshipToken().issueLength(), deployBaseGoerli.issueLength());
     }
 
     /// @notice Test that the Authorship Token's authors were set.
     function test_authorshipTokenAuthorsEquality() public {
-        uint256 totalSupply = deployConstellation.authorshipToken().totalSupply();
-        assertEq(totalSupply, deployConstellation.authorsLength());
+        uint256 totalSupply = deployBaseGoerli.authorshipToken().totalSupply();
+        assertEq(totalSupply, deployBaseGoerli.authorsLength());
 
         unchecked {
             for (uint256 i; i < totalSupply; ++i) {
-                assertEq(
-                    deployConstellation.authorshipToken().ownerOf(i + 1),
-                    deployConstellation.authors(i)
-                );
+                assertEq(deployBaseGoerli.authorshipToken().ownerOf(i + 1), deployBaseGoerli.authors(i));
             }
         }
     }
 
     /// @notice Test that an Authorship Token can be minted after deploy.
     function test_authorshipTokenMinting() public {
-        AuthorshipToken authorshipToken = deployConstellation.authorshipToken();
+        AuthorshipToken authorshipToken = deployBaseGoerli.authorshipToken();
 
         // Warp 1 `issueLength` period forward in time to ensure the owner can
         // mint 1.
@@ -85,14 +74,13 @@ contract DeployConstellationTest is Test {
     /// @notice Test that the Authorship Token's ownership was transferred
     /// correctly.
     function test_authorshipTokenOwnerEquality() public {
-        assertEq(
-            deployConstellation.authorshipToken().owner(),
-            deployConstellation.authorshipTokenOwner()
-        );
+        assertEq(deployBaseGoerli.authorshipToken().owner(), deployBaseGoerli.authorshipTokenOwner());
     }
 
     /// @notice Test that Curta's ownership was transferred correctly.
     function test_curtaOwnerEquality() public {
-        assertEq(deployConstellation.curta().owner(), deployConstellation.curtaOwner());
+        assertEq(deployBaseGoerli.curta().owner(), deployBaseGoerli.curtaOwner());
     }
+
 }
+
