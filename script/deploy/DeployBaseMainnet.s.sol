328a7f74df3509ed32dbc4e724a69be3d56a5c8d
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-11-02 11:21:19 -0500

    add scripts for Base deployment

---
+++ script/deploy/DeployBaseMainnet.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import { DeployBase } from "./DeployBase.s.sol";
+
+contract DeployBaseMainnet is DeployBase {
+    /// @notice The address to transfer the ownership of the Authorship Token
+    /// to.
+    address constant AUTHORSHIP_TOKEN_OWNER = 0xA85572Cd96f1643458f17340b6f0D6549Af482F5;
+
+    /// @notice The address to transfer the ownership of Curta to.
+    address constant CURTA_OWNER = 0xA85572Cd96f1643458f17340b6f0D6549Af482F5;
+
+    /// @notice The number of seconds until an additional token is made
+    /// available for minting by the author.
+    uint256 constant ISSUE_LENGTH = 3 days;
+
+    /// @notice The list of authors in the initial batch.
+    address[] internal AUTHORS = [
+        // fiveoutofnine.eth
+        0xA85572Cd96f1643458f17340b6f0D6549Af482F5,
+        // sabnock.eth
+        0xDbAacdcadD7c51a325B771ff75B261a1e7baE11c
+    ];
+
+    constructor() DeployBase(AUTHORSHIP_TOKEN_OWNER, CURTA_OWNER, ISSUE_LENGTH, AUTHORS) { }
+}
